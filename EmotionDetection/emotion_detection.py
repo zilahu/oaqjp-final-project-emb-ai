14aaf232bf81115c149e96842f1872bab7bb46a1
Kokai Laszlo <kokai.laszlo@gmail.com>
2025-06-13 13:37:19 -0400

    Task 7

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -15,17 +15,27 @@ def emotion_detector(text_to_analyse):
     response = requests.post(url, json = myobj, headers=header)
     response_text = json.loads(response.text)
 
-    emotions = response_text['emotionPredictions'][0]['emotion']
-    dominant_emotion = max(emotions, key=emotions.get)
-    output = {
-        'anger': emotions['anger'],
-        'disgust': emotions['disgust'],
-        'fear': emotions['fear'],
-        'joy': emotions['joy'],
-        'sadness': emotions['sadness'],
-        'dominant_emotion': dominant_emotion,
-    }
-    
+    if response.status_code == 200:
+        emotions = response_text['emotionPredictions'][0]['emotion']
+        dominant_emotion = max(emotions, key=emotions.get)
+        output = {
+            'anger': emotions['anger'],
+            'disgust': emotions['disgust'],
+            'fear': emotions['fear'],
+            'joy': emotions['joy'],
+            'sadness': emotions['sadness'],
+            'dominant_emotion': dominant_emotion,
+        }
+    elif response.status_code == 400:
+        output = {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None,
+        }
+
     return output
 
 
--- server.py
+++ server.py
@@ -8,17 +8,16 @@ app = Flask("Emotion Detector")
 def detect_that_emotion():
     text_to_analyze = request.args.get('textToAnalyze')
     result = emotion_detector(text_to_analyze)
-    if result['dominant_emotion']:
+    if result['dominant_emotion'] == None: 
+        output = "<strong>Invalid text! Please try again!<strong>"
+    else:
         scores = f"'anger': {result['anger']}, 'disgust': {result['disgust']}, 'fear': {result['fear']}, 'joy': {result['joy']}, 'sadness': {result['sadness']}"
         output = f"For the given statement, the system response is {scores}<br>The dominant emotion is <strong>{result['dominant_emotion']}</strong>"
-    else:
-        output = "Invalid input! Try again."
-
     return output
-
 @app.route("/")
 def render_index_page():
     return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+    
